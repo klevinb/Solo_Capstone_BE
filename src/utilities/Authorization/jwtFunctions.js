72be24ec20ae20d77eca1aa06854e15f279aceee
Klevin Bazaiti <klevin.bazaiti98@gmail.com>
2020-09-16 17:23:38 +0200

    auth and jwt token functions

---
+++ src/utilities/Authorization/jwtFunctions.js
@@ -0,0 +1,43 @@
+const jwt = require('jsonwebtoken');
+const UserModel = require('../../routes/users/schema');
+
+const generateToken = async (user) => {
+  try {
+    const token = await jwtCreator({ _id: user._id });
+    const updateUser = await UserModel.findById(user._id);
+
+    updateUser.token = token;
+    await updateUser.save();
+    return { token };
+  } catch (error) {
+    console.log(error);
+    throw new Error(error);
+  }
+};
+
+const jwtCreator = (payload) =>
+  new Promise((res, rej) =>
+    jwt.sign(
+      payload,
+      process.env.SECRET_KEY,
+      { expiresIn: 28800 },
+      (err, token) => {
+        if (err) rej(err);
+        res(token);
+      }
+    )
+  );
+
+const jwtVerifier = (payload) => {
+  new Promise((res, rej) =>
+    jwt.verify(payload, process.env.SECRET_KEY, (err, credentials) => {
+      if (err) rej(err);
+      res(credentials);
+    })
+  );
+};
+
+module.exports = {
+  generateToken,
+  jwtVerifier,
+};
