0c9a900c0232715c9196d34b1de78c0b8b9c9e4b
klevinb <klevin.bazaiti98@gmail.com>
2020-09-18 01:00:04 +0200

    login route created and generateToken function called

--- src/routes/users/index.js
+++ src/routes/users/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const UserModel = require('./schema');
+const { generateToken } = require('../../utilities/Authorization/jwtFunctions');
 
 const router = express.Router();
 
@@ -7,10 +8,28 @@ router.get('/', async (req, res, next) => {
   console.log('HERE');
 });
 
+router.post('/login', async (req, res, next) => {
+  try {
+    const { credentials, password } = req.body;
+
+    const user = await UserModel.findByCredentials(credentials, password);
+
+    if (user) {
+      const token = await generateToken(user);
+      res.send(token);
+    } else {
+      const err = new Error();
+      err.httpStatusCode = 404;
+      err.message = 'User not found!';
+      next(err);
+    }
+  } catch (e) {}
+});
+
 router.post('/register', async (req, res, next) => {
   try {
     const newUser = new UserModel(req.body);
-    const {_id} = await newUser.save();
+    const { _id } = await newUser.save();
     res.status(201).send(_id);
   } catch (error) {
     console.log(error);
