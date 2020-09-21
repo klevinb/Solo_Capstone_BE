f7e34182aaaae4534b7352b353b89edca22156a3
klevinb <klevin.bazaiti98@gmail.com>
2020-09-21 13:26:57 +0200

    added endpoint for events

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const userRoutes = require('./users');
+const eventRoutes = require('./events');
 
 const router = express.Router();
 
 router.use('/users', userRoutes);
+router.use('/events', eventRoutes);
 
 module.exports = router;
